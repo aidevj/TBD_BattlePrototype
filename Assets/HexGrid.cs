c7d3d2e91329fada81333a9bf9cc87868237ddda
Aiden Melendez <itm5305@rit.edu>
2018-04-21 19:30:39 -0400

    github hates me

---
+++ Assets/HexGrid.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Collections;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+	public int width = 6;
+	public int height = 6;
+
+	public HexCell cellPrefab;
+	public Text cellLabelPrefab;
+
+	Canvas gridCanvas;
+	HexMesh hexMesh;
+
+	HexCell[] cells;
+
+	void Awake () {
+		cells = new HexCell[height * width];
+
+		gridCanvas = GetComponentInChildren<Canvas> ();
+		hexMesh = GetComponentInChildren<HexMesh> ();
+
+		for (int z = 0, i = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	void Start(){
+		// after the grid has awoken
+		hexMesh.Triangulate (cells);
+	}
+
+	/// <summary>
+	/// Creates cells at the given positions.
+	/// </summary>
+	/// <param name="x">The x position.</param>
+	/// <param name="z">The z position.</param>
+	/// <param name="i">Index of the cell out of all.</param>
+	void CreateCell (int x, int z, int i) {
+		Vector3 position;
+		// space them out according to radius and offset
+		position.x = ( x + z * 0.5f  - z / 2 ) * (HexMetrics.innerRadius * 2f); //position.x = x * 10f; // <-- non shifted
+		position.y = 0f;
+		position.z = z * (HexMetrics.outerRadius * 1.5f); //position.z = z * 10f;
+
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+
+		// instantiate the labels and show cell coordinates
+		Text label = Instantiate<Text>(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition =
+			new Vector2(position.x, position.z);
+		label.text = x.ToString() + "\n" + z.ToString();
+	}
+
+}
+
+// reference: http://catlikecoding.com/unity/tutorials/hex-map/part-1/
---
+++ Assets/HexMesh.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour {
+
+	Mesh hexMesh;
+	List<Vector3> vertices;
+	List<int> triangles;
+
+	void Awake () {
+		GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+		hexMesh.name = "Hex Mesh";
+		vertices = new List<Vector3>();
+		triangles = new List<int>();
+	}
+
+	/// <summary>
+	/// Triangulate the specified cells. This can be invoked at any time, even when cells have already been triangulated earlier. 
+	/// This will clear the old data then loop through all the cells and triangulate them individually.
+	/// </summary>
+	/// <param name="cells">Cells.</param>
+	public void Triangulate (HexCell[] cells) {
+		hexMesh.Clear();
+		vertices.Clear();
+		triangles.Clear();
+		for (int i = 0; i < cells.Length; i++) {
+			Triangulate(cells[i]);
+		}
+		hexMesh.vertices = vertices.ToArray();
+		hexMesh.triangles = triangles.ToArray();
+		hexMesh.RecalculateNormals();
+	}
+
+	/// <summary>
+	/// Triangulate a single cell.
+	/// </summary>
+	void Triangulate (HexCell cell) {
+		Vector3 center = cell.transform.localPosition;
+		// loop through all six triangles
+		for (int i = 0; i < 6; i++) {
+			AddTriangle (
+				center,
+				center + HexMetrics.corners [i],
+				center + HexMetrics.corners [i + 1]
+			);
+		}
+	}
+
+	/// <summary>
+	/// Adds triangles conviniently, given three vertex positions, adds them order.
+	/// The index of the first vertex is equal to the length of the vertex list before adding the new vertices to it.
+	/// </summary>
+	/// <param name="v1">Vertex location 1.</param>
+	/// <param name="v2">Vertex location 2.</param>
+	/// <param name="v3">Vertex location 3.</param>
+	void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+	}
+}
